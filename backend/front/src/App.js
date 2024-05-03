b113185363d173fb2c18089d8cad8085aaff8862
kykk0 <kykkozy@gmail.com>
2024-05-03 12:51:14 +0300

    done 10 lab

--- backend/front/src/App.js
+++ backend/front/src/App.js
@@ -1,24 +1,38 @@
-import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import NavigationBar from "./components/NavigationBarClass";
+import {BrowserRouter, Routes, Route, Navigate} from "react-router-dom";
+import {connect} from "react-redux";
+
+import NavigationBar from "./components/NavigationBar";
 import Home from "./components/Home";
 import Login from "./components/Login";
+import Utils from "./utils/Utils";
 
-function App() {
-    return (
-        <div className="App">
-            <BrowserRouter>
-                <NavigationBar/>
-                <div className="container-fluid">
-                    <Routes>
-                        <Route path="home" element={<Home />}/>
-                        <Route path="login" element={<Login/>}/>
-                    </Routes>
-                </div>
-            </BrowserRouter>
-        </div>
-    );
+const ProtectedRoute = ({children}) => {
+    let user = Utils.getUser();
+    return user ? children : <Navigate to={'/login'} />
 }
-export default App;
+
+function App(props) {
+  return (
+      <div className="App">
+        <BrowserRouter>
+          <NavigationBar />
+          <div className="container-fluid">
+            {props.error_message &&
+                  <div className="alert alert-danger m-1">{props.error_message}</div>}
+            <Routes>
+                <Route path="login" element={<Login />} />
+                <Route path="home" element={<ProtectedRoute><Home/></ProtectedRoute>}/>
+            </Routes>
+          </div>
+        </BrowserRouter>
+      </div>
+  );
+}
+
+function mapStateToProps(state) {
+    const { msg } = state.alert;
+    return { error_message: msg };
+}
+
+export default connect(mapStateToProps)(App);
